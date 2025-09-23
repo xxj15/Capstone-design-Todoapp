a3d205a7352986b175b65b164f6e22bf4cbafc5d
xxj15 <seojini00@naver.com>
2025-09-24 00:39:36 +0900

    mainpage 배경색 설정

---
+++ flutter_todo/lib/main_page.dart
@@ -0,0 +1,16 @@
+// 메인 페이지
+
+// 메인 페이지는 로고, 텍스트, 버튼만 표시됨 (내용이 고정되어 있음) => StatelessWidget 사용을 사용. 
+class MainPage extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFEDE6), 
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
