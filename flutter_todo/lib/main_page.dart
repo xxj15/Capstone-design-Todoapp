f0fca6baa922551629b9a4910c4fe977e23055f8
xxj15 <seojini00@naver.com>
2025-09-24 01:44:34 +0900

    made by. @xxj15 추가 후 링크 연결

--- flutter_todo/lib/main_page.dart
+++ flutter_todo/lib/main_page.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-// 메인 페이지는 로고, 텍스트, 버튼만 표시됨 (내용이 고정되어 있음) => StatelessWidget 사용 
-class MainPage extends StatelessWidget { 
+// 메인 페이지는 로고, 텍스트, 버튼만 표시됨 (내용이 고정되어 있음) => StatelessWidget 사용
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFEDE6), 
+      backgroundColor: Color(0xFFFFEDE6),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // 로고 이미지 (가운데)
           Image.asset(
             'assets/logo(short).png',
-            width: 300,
-            height: 300,
+            width: 250,
+            height: 250,
           ),
-          SizedBox(height: 20),// sizedbox : CSS의 margin과 유사
-          Column(
-            children: [
-              Text(
-                '하루하루 해내는 즐거움,\n마이 To-Do',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontFamily: 'OngleipRyuryu',
-                ),
-              ),
-            ],
+          SizedBox(height: 20), // SizedBox : CSS의 margin과 유사
+          
+          Text(
+            '하루하루 해내는 즐거움,\n마이 To-Do',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+              fontFamily: 'OngleipRyuryu',
+            ),
           ),
           SizedBox(height: 40),
+          
+          // "오늘의 할 일 추가하기" 버튼
           Container(
             width: 280,
             height: 50,
             decoration: BoxDecoration(
               color: Color(0xFFFF9D8C),
-              borderRadius: BorderRadius.circular(25),
+              borderRadius: BorderRadius.circular(10),
               boxShadow: [
                 BoxShadow(
                   color: Colors.pink[200]!,
@@ -48,13 +48,45 @@ class MainPage extends StatelessWidget {
               child: Text(
                 '오늘의 할 일 추가하기',
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 22,
                   color: Colors.white,
                   fontWeight: FontWeight.w500,
                   fontFamily: 'OngleipRyuryu',
                 ),
               ),
             ),
+          ),
+          SizedBox(height: 60),
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Made by. ',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontFamily: 'OngleipRyuryu',
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  final url = Uri.parse('https://github.com/xxj15');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  }
+                },
+                child: Text(
+                  '@xxj15',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue[600],
+                    fontFamily: 'OngleipRyuryu',
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
