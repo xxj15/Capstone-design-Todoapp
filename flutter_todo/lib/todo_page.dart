1537d8a137fdff9ba8565adfda4362ee0b1bb0b4
xxj15 <seojini00@naver.com>
2025-09-24 02:21:11 +0900

    투두페이지 기본 레이아웃 생성

--- flutter_todo/lib/todo_page.dart
+++ flutter_todo/lib/todo_page.dart
@@ -1,1 +1,22 @@
 // 투두 페이지
+
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFEDE6),
+      body: Center(
+        child: Text(
+          '투두페이지입니다',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+            fontFamily: 'OngleipRyuryu',
+          ),
+        ),
+      ),
+    );
+  }
+}
