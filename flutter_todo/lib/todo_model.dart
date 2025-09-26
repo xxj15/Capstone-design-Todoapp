2dc0c021289beffb5a46f24394b96b7f334f5095
xxj15 <seojini00@naver.com>
2025-09-26 14:06:43 +0900

    TodoModel 클래스 정의

--- flutter_todo/lib/todo_model.dart
+++ flutter_todo/lib/todo_model.dart
@@ -1,1 +1,13 @@
-// 할 일 데이터 - 할 일 정의 
+
+class TodoModel {
+  final String id; // 할 일의 고유 식별자(id)
+  final String text; // 할 일 내용
+  final bool isCompleted; // 완료 여부
+
+// todo_page.dart에서 이제 List<String> _todos 대신 List<TodoModel> _todos 를 사용할 수 있다.
+  TodoModel({
+    required this.id,
+    required this.text,
+    required this.isCompleted,
+  });
+}
